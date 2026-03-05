a830ea8af8193acd16a6b4d9f113175e0c9a062f
dhasociados25-creator <dhasociados25@gmail.com>
2026-03-05 01:37:28 -0300

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,27 @@
+const productos = [
+
+{
+nombre: "Cuaderno universitario",
+precio: 1990,
+stock: 4
+},
+
+{
+nombre: "Resma papel carta",
+precio: 4990,
+stock: 10
+},
+
+{
+nombre: "Lápiz grafito",
+precio: 120,
+stock: 50
+},
+
+{
+nombre: "Pegamento en barra",
+precio: 890,
+stock: 0
+}
+
+];
